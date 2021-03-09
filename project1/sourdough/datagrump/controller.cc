16f602d8713340d0f86c3c87be272d955ac0ec6e
Elwin Stephan <elwin.stephan@gmail.com>
2021-03-09 16:36:33 +0100

    add more serious aimd

--- project1/sourdough/datagrump/controller.cc
+++ project1/sourdough/datagrump/controller.cc
@@ -6,7 +6,7 @@
 using namespace std;
 
 /* Default constructor */
-Controller::Controller(const bool debug) : debug_(debug), window_size_(5), last_ack_(0) {
+Controller::Controller(const bool debug) : debug_(debug), cwnd_(1), thresh_(1000) {
 }
 
 /* Get current window size, in datagrams */
@@ -18,10 +18,10 @@ unsigned int Controller::window_size() {
 
     if (debug_) {
         cerr << "At time " << timestamp_ms()
-             << " window size is " << window_size_ << endl;
+             << " window size is " << cwnd_ << endl;
     }
 
-    return window_size_;
+    return cwnd_;
 }
 
 /* A datagram was sent */
@@ -31,7 +31,10 @@ void Controller::datagram_was_sent(const uint64_t sequence_number,
         /* in milliseconds */
                                    const bool after_timeout
         /* datagram was sent because of a timeout */ ) {
-    if (after_timeout) window_size_ = 1;
+    if (after_timeout) {
+        thresh_ = cwnd_ / 2;
+        cwnd_ = 1;
+    }
 
     if (debug_) {
         cerr << "At time " << send_timestamp
@@ -49,11 +52,12 @@ void Controller::ack_received(const uint64_t sequence_number_acked,
                               const uint64_t timestamp_ack_received)
 /* when the ack was received (by sender) */
 {
-    if (sequence_number_acked >= last_ack_) {
-        last_ack_ = sequence_number_acked;
-        window_size_++;
+    if (cwnd_ < thresh_) {
+        cwnd_++;
+    } else {
+//        cwnd_ = cwnd_ + 1 / cwnd_;
+        cwnd_++;
     }
-
 
     if (debug_) {
         cerr << "At time " << timestamp_ack_received
--- project1/sourdough/datagrump/controller.hh
+++ project1/sourdough/datagrump/controller.hh
@@ -5,40 +5,39 @@
 
 /* Congestion controller interface */
 
-class Controller
-{
+class Controller {
 private:
-  bool debug_; /* Enables debugging output */
-  uint64_t window_size_;
-  uint64_t last_ack_;
+    bool debug_; /* Enables debugging output */
+    uint64_t cwnd_;
+    uint64_t thresh_;
 
-  /* Add member variables here */
+    /* Add member variables here */
 
 public:
-  /* Public interface for the congestion controller */
-  /* You can change these if you prefer, but will need to change
-     the call site as well (in sender.cc) */
+    /* Public interface for the congestion controller */
+    /* You can change these if you prefer, but will need to change
+       the call site as well (in sender.cc) */
 
-  /* Default constructor */
-  Controller( const bool debug );
+    /* Default constructor */
+    Controller(const bool debug);
 
-  /* Get current window size, in datagrams */
-  unsigned int window_size();
+    /* Get current window size, in datagrams */
+    unsigned int window_size();
 
-  /* A datagram was sent */
-  void datagram_was_sent( const uint64_t sequence_number,
-			  const uint64_t send_timestamp,
-			  const bool after_timeout );
+    /* A datagram was sent */
+    void datagram_was_sent(const uint64_t sequence_number,
+                           const uint64_t send_timestamp,
+                           const bool after_timeout);
 
-  /* An ack was received */
-  void ack_received( const uint64_t sequence_number_acked,
-		     const uint64_t send_timestamp_acked,
-		     const uint64_t recv_timestamp_acked,
-		     const uint64_t timestamp_ack_received );
+    /* An ack was received */
+    void ack_received(const uint64_t sequence_number_acked,
+                      const uint64_t send_timestamp_acked,
+                      const uint64_t recv_timestamp_acked,
+                      const uint64_t timestamp_ack_received);
 
-  /* How long to wait (in milliseconds) if there are no acks
-     before sending one more datagram */
-  unsigned int timeout_ms();
+    /* How long to wait (in milliseconds) if there are no acks
+       before sending one more datagram */
+    unsigned int timeout_ms();
 };
 
 #endif
