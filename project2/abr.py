eba2a82000d6bcb5f3cf2c8f771a345c8241c587
Tobias Buner <bunert@student.ethz.ch>
2021-04-06 22:25:01 +0200

    105.279

--- project2/abr.py
+++ project2/abr.py
@@ -1,15 +1,18 @@
 from enum import Enum
 
 
-class CALLBACK_EVENT( Enum ):
+class CALLBACK_EVENT(Enum):
     INIT = 0
     DOWNLOAD_COMPLETED = 1
-    TIMEOUT  = 2
+    TIMEOUT = 2
     REBUFFERING = 3
 
 
 last_chunk = 0
 byte_downloaded = 0
+next_bitrate = 0
+BITRATES = [300, 750, 1200, 1850, 2850, 4300] # Kbps
+
 
 def abr(
         typ,
@@ -27,9 +30,9 @@ def abr(
             DOWNLOAD_COMPLETED - a chunk has been downloaded
             TIMEOUT - timeout has happend
             REBUFFERING - rebuffering started
-        
+
         current_time - time from the beginning of the simulation in seconds
-        
+
         playback_time - how much of the video has been shown (in seconds)
         playback_chunk - the chunk that is playing right now
         current_chunk - the chunk number that is downloading right now (or has been just finished)
@@ -53,9 +56,9 @@ def abr(
                         - timeout is in absolute time, usually set it as current_time+X (where min X is 200ms)
                         - timeout 0 means no timeout
     """
-    global last_chunk, byte_downloaded
-    
-    #initial
+    global last_chunk, byte_downloaded, next_bitrate
+
+    # initial
     if typ == CALLBACK_EVENT.INIT:
         last_chunk = 0
         return 0, 0, current_time + 0.2
@@ -63,7 +66,7 @@ def abr(
     # print("current chunk downloaded: ", current_chunk_download)
     # print("total size: ", video[current_chunk_quality][current_chunk])
 
-    #rebuffering or timeout, ignore 
+    #rebuffering or timeout, ignore
     if typ == CALLBACK_EVENT.DOWNLOAD_COMPLETED:
         # print("complete, quality: ", current_chunk_quality)
         next_chunk = current_chunk + 1
@@ -72,30 +75,54 @@ def abr(
 
         if next_chunk == len(video[0]):
             return 0, -1, 0.0
-        
-        # increase
-        if current_chunk - playback_chunk > 3:
-            next_chunk_quality = min(current_chunk_quality + 1, 5)
+
+        buf = current_chunk - playback_chunk
         # decrease
-        elif current_chunk - playback_chunk < 3:
-            next_chunk_quality = max(current_chunk_quality - 1, 0)
+        if buf < 3:
+            next_chunk_quality = min(max(current_chunk_quality - 1, 0), next_bitrate)
+        # increase
+        elif buf >= 5:
+            next_chunk_quality = max(min(current_chunk_quality + 1, 5), next_bitrate)
         else:
-            next_chunk_quality = current_chunk_quality
+            next_chunk_quality = max(current_chunk_quality, next_bitrate)
+        
+        # print("next chunk: ", video[next_chunk_quality][next_chunk])
 
         return next_chunk_quality, next_chunk, current_time + 0.2
 
     # Rebuffering, fall back to the lowest quality
     if typ == CALLBACK_EVENT.REBUFFERING:
-        return 0, current_chunk, current_time + 0.2
+        # if bytes left of current_chunt_quality is less than lowest quality don't change quality
+        if (video[current_chunk_quality][current_chunk] - current_chunk_download < video[0][current_chunk]):
+            # print("rebuffering, but finish actual chunk")
+            return current_chunk_quality, current_chunk, current_time + 0.2
+        else:
+            # print("rebuffering, start new chunk with quality 0")
+            byte_downloaded = 0
+            return 0, current_chunk, current_time + 0.2
 
     # Trigger
     if typ == CALLBACK_EVENT.TIMEOUT:
-        current_bandwidth = current_chunk_download - byte_downloaded
-        # print("bw (last 200ms): ", current_bandwidth)
-
-
+        # bytes downloaded in the last 200 ms
+        number_of_bytes = current_chunk_download - byte_downloaded
+        # already downloaded bytes of current chunk
         byte_downloaded = current_chunk_download
+        # capacity of the last 200ms [Kbps]
+        current_capacity = (number_of_bytes * 5 * 8)/1000
+
+        i = 0
+        while i < len(BITRATES):
+            if BITRATES[i] >= current_capacity:
+                break
+            i += 1
+
+        next_bitrate = max(i - 1, 0)
+        # print("bytes: " +  str(number_of_bytes) + "\t bps: " + str(number_of_bytes*5) + "\t current bitrate: " + str(BITRATES[current_chunk_quality]*1000))
+        # print("chunk: " + str(current_chunk) + 
+        #         "\t current capacity: " + "{:10.4f} [Kbps]".format(current_capacity) + 
+        #         "\t quality: " + str(current_chunk_quality) + 
+        #         "\t buf: " + str(current_chunk - playback_chunk) +
+        #         "\t next: " + str(next_bitrate)) 
 
 
         return current_chunk_quality, current_chunk, current_time + 0.2
-
