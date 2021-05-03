e2e78441b6d5863b05feabb4ec5dc0e2323a7225
Elwin Stephan <elwin.stephan@gmail.com>
2021-05-03 16:09:04 +0200

    add brute force djikstra

--- project3/scripts/solve.py
+++ project3/scripts/solve.py
@@ -1,6 +1,14 @@
 import pandas as pd
-from util import *
+import util
+import networkx as nx
 from random import randrange
+
+sat_pos_file = "../input_data/sat_positions.txt"
+city_pos_file = "../input_data/cities.txt"
+city_coverage_file = "../input_data/city_coverage.txt"
+city_pair_file = "../input_data/city_pairs.txt"
+valid_isls_file = "../input_data/valid_isls.txt"
+sat_links_file = "../output_data/sat_links.txt"
 
 
 def read_satellites() -> pd.DataFrame:
@@ -21,7 +29,7 @@ def read_satellites() -> pd.DataFrame:
 
 
 def read_isls() -> dict:
-    return read_valid_isls(
+    return util.read_valid_isls(
         '../input_data/valid_isls.txt',
     )
 
@@ -105,10 +113,62 @@ def full_grid():
     return dict_to_list(mapping)
 
 
-if __name__ == '__main__':
-    result = snake()
-    # result = full_grid()
+def is_satellite(hop_id: int) -> bool:
+    return hop_id < 1600
 
+
+def build_graph(city_positions, city_coverage, sat_positions, valid_isls, sat_conn_count):
+    g = nx.Graph()
+
+    for city in city_positions:
+        g.add_node(city)
+    for city, sat in city_coverage:
+        g.add_edge(city, sat, length=city_coverage[city, sat])
+
+    for sat in sat_positions:
+        g.add_node(sat)
+    for key, value in valid_isls.items():
+        if sat_conn_count[key[0]] < 4 and sat_conn_count[key[1]] < 4:
+            g.add_edge(key[0], key[1], length=value)
+
+    return g
+
+
+def djikstra():
+    city_positions = util.read_city_positions(city_pos_file)
+    city_coverage = util.read_coverage(city_coverage_file)
+    sat_positions = util.read_sat_positions(sat_pos_file)
+    valid_isls = util.read_valid_isls(valid_isls_file)
+    city_pairs = util.read_city_pairs(city_pair_file)
+
+    connections = {}
+    sat_conn_count = [0 for _ in range(1600)]
+
+    for i in range(10001, 10100):
+        for j in range(i + 1, 10101):
+            g = build_graph(city_positions, city_coverage, sat_positions, valid_isls, sat_conn_count)
+
+            path = nx.single_source_dijkstra_path(g, i, weight='length')[j]
+
+            cur_hop = path[0]
+            for next_hop in path[1:]:
+                if is_satellite(cur_hop) and is_satellite(next_hop):
+                    connections[(cur_hop, next_hop)] = True
+                    sat_conn_count[cur_hop] += 1
+                    sat_conn_count[next_hop] += 1
+
+                cur_hop = next_hop
+
+            print(f"connected {i} to {j}")
+
+    return dict_to_list(connections)
+
+
+if __name__ == '__main__':
+    # result = snake()
+    # result = full_grid()
+    result = djikstra()
+    
     write_mapping(result)
 
     exec(open("check_score.py").read())  # who needs modules anyway
